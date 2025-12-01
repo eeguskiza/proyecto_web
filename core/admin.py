db158aa8be45292009b1f9b49d32f8c4c6cd372c
jonVelasco13 <jon.velasco.s@opendeusto.es>
2025-12-01 13:22:53 +0100

    implementacion admin

--- core/admin.py
+++ core/admin.py
@@ -17,10 +17,38 @@ admin.site.register(Species)
 admin.site.register(Planet)
 admin.site.register(Media)
 admin.site.register(Affiliation)
-admin.site.register(Character)
 admin.site.register(Appearance)
 admin.site.register(CharacterAffiliation)
 admin.site.register(Region)
 admin.site.register(Sector)
 admin.site.register(PlanetSpecies)
 admin.site.register(StarSystem)
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("name", "species", "homeworld", "display_affiliations")
+    list_filter = ("species", "homeworld")
+    search_fields = ("name", "species__name", "homeworld__name")
+    
+    # Campos calculados o relaciones ManyToMany
+    def display_affiliations(self, obj):
+        return ", ".join([a.name for a in obj.affiliations.all()])
+    display_affiliations.short_description = "Affiliations"
+    
+    # Control de permisos: ocultar campos para usuarios no superuser
+    def get_fields(self, request, obj=None):
+        fields = ["name", "species", "homeworld", "gender", "image_url"]
+        if request.user.is_superuser:
+            fields.append("secret_notes")  # solo visible para superusers
+        return fields
+    
+    # También se pueden quitar columnas según el usuario
+    def get_list_display(self, request):
+        columns = list(self.list_display)
+        if not request.user.is_superuser:
+            # quitar columna que solo admins pueden ver
+            columns.remove("display_affiliations")
+        return columns
+
+# Registrar el admin
+admin.site.register(Character, CharacterAdmin)
